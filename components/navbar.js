4da9dcad8eaa71668dd52978b0b80a77d2bcf799
Mojahid213 <mojoffi133@gmail.com>
2021-11-18 12:33:25 +0600

    Client-side Authentication

--- components/navbar.js
+++ components/navbar.js
@@ -1,20 +1,26 @@
 import Link from "next/link";
-import { signIn, signOut } from "next-auth/client";
+import { signIn, signOut, useSession } from "next-auth/client";
 
 export default function Navbar() {
+  const [session, loading] = useSession();
+  console.log({ session, loading });
   return (
     <>
       <div>
         <nav className="navbar">
           <h1 className="logo-icon">NextAuth</h1>
-          <ul className="menu-lists">
+          <ul
+            className={`menu-lists ${
+              !session && loading ? "loading" : "loaded"
+            }`}
+          >
             <li>
               <Link href="/">
                 <a>Home</a>
               </Link>
             </li>
             <li>
-              <Link href="dashboard">
+              <Link href="/dashboard">
                 <a>Dashboard</a>
               </Link>
             </li>
@@ -23,30 +29,34 @@ export default function Navbar() {
                 <a>Blog</a>
               </Link>
             </li>
-            <li>
-              <Link href="/api/auth/signin">
-                <a
-                  onClick={(e) => {
-                    e.preventDefault();
-                    signIn("github");
-                  }}
-                >
-                  Sign In
-                </a>
-              </Link>
-            </li>
-            <li>
-              <Link href="/api/auth/sighout">
-                <a
-                  onClick={(e) => {
-                    e.preventDefault();
-                    signOut();
-                  }}
-                >
-                  Sign Out
-                </a>
-              </Link>
-            </li>
+            {!loading && !session && (
+              <li>
+                <Link href="/api/auth/signin">
+                  <a
+                    onClick={(e) => {
+                      e.preventDefault();
+                      signIn("github");
+                    }}
+                  >
+                    Sign In
+                  </a>
+                </Link>
+              </li>
+            )}
+            {session && (
+              <li>
+                <Link href="/api/auth/sighout">
+                  <a
+                    onClick={(e) => {
+                      e.preventDefault();
+                      signOut();
+                    }}
+                  >
+                    Sign Out
+                  </a>
+                </Link>
+              </li>
+            )}
           </ul>
         </nav>
       </div>
